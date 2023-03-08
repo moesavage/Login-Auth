12b3dcd800aa532ac4a21ff082b713332247f192
Folarin Morolade <moroladefolarin2@gmail.com>
2023-03-09 00:56:42 +0100

    Add files via upload

---
+++ Controllers/UserAuthenticationController.cs
@@ -0,0 +1,85 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Authorole.Models.DTO;
+using Authorole.Repositories.Abstract;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Authorole.Controllers
+{
+    public class UserAuthenticationController : Controller
+    {
+        private readonly IUserAuthenticationService _service;
+
+        public UserAuthenticationController(IUserAuthenticationService service)
+        {
+            this._service = service;
+        }
+
+        public ActionResult Registration()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task <IActionResult> Registration(RegistrationModel model)
+        {
+            if (!ModelState.IsValid)
+                return View(model);
+            model.Role = "user";
+            var result = await _service.RegisterAsync(model);
+            TempData["msg"] = result.Message;
+            return RedirectToAction(nameof(Login));
+
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            var result = await _service.LoginAsync(model);
+            if (result.StatusCode == 1)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                TempData["msg"] = result.Message;
+                return RedirectToAction(nameof(Login));
+            }
+        }
+
+        [Authorize]
+        public async Task<IActionResult> Logout()
+        {
+            await _service.LogoutAsync();
+            return RedirectToAction(nameof(Login));
+        }
+
+        //public async Task<IActionResult> Reg()
+        //{
+        //    var model = new RegistrationModel
+        //    {
+        //        Username = "admin1",
+        //        Name = "Moro savage",
+        //        Email = "morosavage@gmail.com",
+        //        Password = "Admin@1234"
+        //    };
+        //    model.Role = "admin";
+        //    var result = await _service.RegisterAsync(model);
+        //    TempData["msg"] = result.Message;
+        //    return Ok(result);
+        //}
+    }
+}
